3feac4662f0afbd5dd688b20ce541c0e376434fb
rbrederode <rbrederode@yahoo.co.uk>
2026-04-06 10:39:47 +0100

    Changed script ip address to use ethernet @ home

--- src/sdp001.sh
+++ src/sdp001.sh
@@ -1,1 +1,1 @@
-python -m sdp.sdp --entity_id sdp001 --tm_host 192.168.0.2 --dig_host 192.168.0.2 --scan_store_dir ~/samples
+python -m sdp.sdp --entity_id sdp001 --tm_host 192.168.0.7 --dig_host 192.168.0.7 --scan_store_dir ~/samples
--- src/tm001.sh
+++ src/tm001.sh
@@ -1,1 +1,1 @@
-python tm/tm.py --entity_id tm001 --dig_host 192.168.0.2 --sdp_host 192.168.0.2 --dm_host 192.168.0.2 --ws_host 192.168.0.18
+python tm/tm.py --entity_id tm001 --dig_host 192.168.0.7 --sdp_host 192.168.0.7 --dm_host 192.168.0.7 --ws_host 192.168.0.18
